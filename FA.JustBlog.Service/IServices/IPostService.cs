3455bf6f086eef6f3b6b4f5a3e82c0a53329bc10
hieumthe1638431 <hieumthe163841@fpt.edu.vn>
2024-02-29 09:03:05 +0700

    update project

---
+++ FA.JustBlog.Service/IServices/IPostService.cs
@@ -0,0 +1,26 @@
+﻿using FA.JustBlog.Entities.Models;
+namespace FA.JustBlog.Service.IServices
+{
+    public interface IPostService
+    {
+        IList<Post> GetMostViewedPost(int size);
+        IList<Post> GetAllEntities();
+        Post Find(int entityId);
+        
+        void AddPost(Post entity);
+        void UpdatePost(Post entity);
+        void DeletePost(Post entity);
+        void DeletePost(int entityId);
+        IList<Post> GetPublisedPosts();
+        IList<Post> GetUnpublisedPosts();
+        IList<Post> GetLatestPost(int size);
+        int CountPostsForCategory(int category);
+        IList<Post> GetPostsByCategory(string categoryName);
+        IList<Post> GetHighestPosts(int size);
+        public IEnumerable<Post> PostPagination(int pageSize, int PageIndex);
+        dynamic GetPostWithCategory();
+        dynamic GetLastestPostWithCategory(int size);
+        dynamic GetMostPostWithCategory(int size);
+
+    }
+}
---
+++ FA.JustBlog.ViewModel/PostViewModel/PostVM.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FA.JustBlog.ViewModel.PostViewModel
+{
+    public class PostVM
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string ShortDecription { get; set; }
+        public string PostContent { get; set; }
+        public string UrlSlug { get; set; }
+        public bool? Published { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<string> TagName { get; set; }
+        public ICollection<int> CommentId { get; set; }
+        public int? ViewCount { get; set; }
+        public DateTime? PostOn { get; set; }
+    }
+}
